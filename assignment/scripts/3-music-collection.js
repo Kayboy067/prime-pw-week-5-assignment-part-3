a701ad753e613f7a3ae20576370d23b8b4a2d324
Kayode Adebanjo <kay.adebanjo@gmail.com>
2021-12-05 14:41:18 -0600

    testing addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,20 @@
 console.log('***** Music Collection *****')
-let Collection = []
+let collection = []
 
-function addToCollection (title, artist, yearPublished){
-    
+function addToCollection(title, artist, yearPublished){
+    let album = {
+        title: title, 
+        artist: artist, 
+        yearPublished: yearPublished,
+    }
+    collection.push(album);
+    return album;
 }
+console.log(addToCollection('Holla At Your Boy', 'Wizkid', 2011));
+console.log(addToCollection('African Giant', 'Burna Boy', 2020));
+console.log(addToCollection('Essence', 'Wizkid feat Tems', 2019));
+console.log(addToCollection('Somebody Son', 'Tiwa Savage feat Brandy', 2021));
+console.log(addToCollection('Fall', 'Davido', 2019));
+console.log(addToCollection('No Guidance', 'Chris Brown feat Drake', 2020));
+console.log(collection); //end test
+
